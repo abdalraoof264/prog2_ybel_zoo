e405de1562d104aa4c7c2a0984d53aead572da2e
Abd Alraoof <abdalraoofkx@gmail.com>
2025-07-04 02:31:43 +0200

    Aufgabe 4 ist Fertig

--- src/main/java/zoo/Main.java
+++ src/main/java/zoo/Main.java
@@ -2,6 +2,8 @@ package zoo;
 
 import zoo.mammals.cats.Lion;
 import zoo.mammals.rodents.Hamster;
+import zoo.reptiles.Turtle;
+import zoo.fishes.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,16 +13,35 @@ public class Main {
         Lion simba = new Lion();
         Lion nala = new Lion();
         Hamster hansi = new Hamster();
+        Habitat<Fish> troutHabitat = new Habitat<>("Forellenbecken");
+        troutHabitat.add(new Trout());
 
         loewengehege.add(simba);
         loewengehege.add(nala);
         loewengehege.add(simba);
 
         hamstergehege.add(hansi);
+        Habitat<Fish> salmonHabitat = new Habitat<>("Lachsbecken");
+        salmonHabitat.add(new Salmon());
 
         loewengehege.remove(nala);
         hamstergehege.remove(new Hamster());
 
         Lion test = new Lion();
+        Aquarium aquarium = new Aquarium();
+        aquarium.build(troutHabitat);
+        aquarium.build(salmonHabitat);
+
+
+        Zoo<Habitat<?>> zoo = new Zoo<>();
+        zoo.build(loewengehege);
+        zoo.build(hamstergehege);
+        Habitat<Turtle> schildkroetengehege = new Habitat<>("Schildkroetengehege");
+
+        zoo.build(schildkroetengehege);
+        zoo.abandon(schildkroetengehege);
+        zoo.visitAllHabitats();
+
+        aquarium.visitAllHabitats();
     }
 }
